f87d300868bdf97a84acff861f5d505ab908fead
cphVB Buildbot <buildbot@cphvb.org>
2012-10-24 18:06:23 +0200

    Added a place to dump benchmark suites.

---
+++ suites/__init__.py
@@ -0,0 +1,6 @@
+"""
+This place is a container for benchmark suites.
+They used to live inside of press.py but as more and more 
+suites kept crawling in it seemed like a good idea to
+give them a place to live.
+"""
