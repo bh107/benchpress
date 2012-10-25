7255c5d29a8866a8dda6f05707ff924291978831
Simon A. F. Lund <safl@safl.dk>
2012-10-25 21:12:21 +0200

    ...

--- suites/tiling_swater.py
+++ suites/tiling_swater.py
@@ -1,18 +1,5 @@
 from default import scripts, engines
-
-block_sizes = [str((2**i-(2**(i-2)/2))) for i in xrange(7,21) ]
-base_maxes  = [str(j) for j in xrange(1,10)]
-
-subset = [
-    ('score_blksize%s_basemax%s' % (blksize, basemax),
-    'score',
-    {
-        "CPHVB_VE_SCORE_BLOCKSIZE": blksize,
-        "CPHVB_VE_SCORE_BINMAX":    str(20),
-        "CPHVB_VE_SCORE_BASEMAX":   basemax,
-        "CPHVB_CORE_MCACHE_SIZE":   10
-    }) for basemax in base_maxes for blksize in block_sizes
-]
+from tiling_search import subset
 
 suite = {
     'scripts':  [script for script in scripts if 'Shallow' in script[0]],
