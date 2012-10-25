f545fd7698c72ec107aaacc7557480036175c889
Simon A. F. Lund <safl@safl.dk>
2012-10-25 13:14:02 +0200

    ...

---
+++ suites/tiling_jacobi.py
@@ -0,0 +1,20 @@
+from default import scripts, engines
+
+block_sizes = [str((2**i-(2**(i-2)/2))) for i in xrange(7,21) ]
+base_maxes  = [str(j) for j in xrange(1,10)]
+
+subset = [
+    ('score_blksize%s' % (blksize),
+    'score',
+    {
+        "CPHVB_VE_SCORE_BLOCKSIZE": blksize,
+        "CPHVB_VE_SCORE_BINMAX":    str(20),
+        "CPHVB_VE_SCORE_BASEMAX":   5,
+        "CPHVB_CORE_MCACHE_SIZE":   10
+    }) blksize in block_sizes
+]
+
+suite = {
+    'scripts': [script for script in scripts if 'Jacobi Iterative - Reduce' in script[0]],
+    'engines': engines[0] + subset
+}
