af6bc62b4a425ca0ab03fcddce45823f46502d5d
Simon A. F. Lund <safl@safl.dk>
2012-10-25 12:25:36 +0200

    ...

--- suites/tiling_base.py
+++ suites/tiling_base.py
@@ -1,5 +1,8 @@
 from default import scripts, engines
 
+# Run on all default-script with numpy, simple and score
+# with default parameters.
+
 suite = {
     'scripts': scripts,
     'engines': engines[0:3]
