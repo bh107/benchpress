d316e2348c7106897eb5d93be176f8f603ff7664
Simon A. F. Lund <safl@safl.dk>
2012-10-27 22:29:49 +0200

    ...

--- suites/tiling_comp.py
+++ suites/tiling_comp.py
@@ -5,11 +5,12 @@ from default import scripts, engines
 
 suite = {
     'scripts': [
-        ('BlackScholes-c',   '../ccode/bscholes',  '2000000 4'),
+        ('BlackScholes-c',  '../ccode/bscholes',  '2000000 4'),
         ('Black Scholes',   'bscholes.py',  '--size=2000000*4'),
-        ('Jacobi-c', '../ccode/jacobi', '7000 4'),
+        ('Jacobi-c',        '../ccode/jacobi', '7000 4'),
         ('Jacobi Iterative - Reduce', 'jacobi.iterative.reduc.py', '--size=7000*7000*4'),
         
     ],
-    'engines': [engines[2]]
+    'engines': [engines[0], engines[2]]
+
 }
