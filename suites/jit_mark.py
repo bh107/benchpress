f33849ff7e0a711fddacc5adfe412959e6561b73
Johannes Lund <johannes.lund@gmail.com>
2012-11-30 13:21:43 +0100

    .

--- suites/jit_mark.py
+++ suites/jit_mark.py
@@ -21,15 +21,21 @@ jacobi_iterative_script = []
 for i in xrange(1,10):
     jacobi_iterative_script.append( ('Jacobi Iterative - Reduce {0}'.format(i),   'jacobi.iterative.reduc.py','--size={0}*{1}*{2}'.format(x,y,i) ))
 for i in xrange(1,11):
-    jacobi_iterative_script.append( ('Jacobi Iterative - Reduce {0}'.format(i),   'jacobi.iterative.reduc.py','--size={0}*{1}*{2}'.format(x,y,i*10) ))
+    jacobi_iterative_script.append( ('Jacobi Iterative - Reduce {0}'.format(i*10),   'jacobi.iterative.reduc.py','--size={0}*{1}*{2}'.format(x,y,i*10) ))
 jacobi_iterative_script.append( ('Jacobi Iterative - Reduce {0}'.format(200),   'jacobi.iterative.reduc.py','--size={0}*{1}*{2}'.format(x,y,200) ))
 
 jacobi_iterative_script_naive = []
 for i in xrange(1,10):
-    jacobi_iterative_script_naive.append( ('Jacobi Iterative - Reduce Naive {0}'.format(i*10),   'jacobi.iterative.reduce.oneline.py','--size={0}*{1}*{2}'.format(x,y,i) ))
+    jacobi_iterative_script_naive.append( ('Jacobi Iterative - Reduce Naive {0}'.format(i),   'jacobi.iterative.reduce.oneline.py','--size={0}*{1}*{2}'.format(x,y,i) ))
 for i in xrange(1,11):
     jacobi_iterative_script_naive.append( ('Jacobi Iterative - Reduce Naive {0}'.format(i*10),   'jacobi.iterative.reduce.oneline.py','--size={0}*{1}*{2}'.format(x,y,i*10) ))
 jacobi_iterative_script_naive.append( ('Jacobi Iterative - Reduce Naive {0}'.format(200),   'jacobi.iterative.reduce.oneline.py','--size={0}*{1}*{2}'.format(x,y,200) ))
+
+
+jacobi_fixed = []
+jacobi_fixed.append( ('Jacobi Iterative - Reduce {0}'.format(1),   'jacobi.iterative.py','--size={0}*{1}*{2}'.format(x,y,1) ))
+for i in xrange(1,11):
+    jacobi_fixed.append( ('Jacobi Iterative - Reduce {0}'.format(i*10),   'jacobi.iterative.py','--size={0}*{1}*{2}'.format(x,y,i*10) ))
 
 
 # Scripts and their arguments
@@ -88,6 +94,6 @@ scripts   = [
 
 suite = {
     'scripts':  jacobi_iterative_script + jacobi_iterative_script_naive,
-    'engines':  engines[0:4] + [engines[6]] + [engines[8]]
+    'engines':  engines[0:2] + [engines[6]] + [engines[8]]
     #'engines':  engines[0:4] + engines[6:10]
 }
