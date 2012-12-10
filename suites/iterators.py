c85e1bfa6b826ab4a629e8dbf489ad303efea6b1
Kenneth Skovhede <kenneth@hexad.dk>
2012-12-10 16:15:17 +0100

    Removed iterator2 and iterator3

--- suites/iterators.py
+++ suites/iterators.py
@@ -2,5 +2,5 @@ from default import scripts, engines
 
 suite = {
     'scripts':  scripts,
-    'engines':  engines[0:4] + [('iterator', 'iterator', None)] + [('iterator2', 'iterator2', None)] + [('iterator3', 'iterator3', None)]
+    'engines':  engines[0:4] + [('iterator', 'iterator', None)]
 }
