21eff3b11065398ac58c07688c4a791f9ba43ff6
Kenneth Skovhede <kenneth@hexad.dk>
2013-01-24 15:20:19 +0100

    Fixed a bug in select_ve.py

--- select_ve.py
+++ select_ve.py
@@ -1,7 +1,7 @@
 from ConfigParser import SafeConfigParser
 
 if __name__ == "__main__":
-	conffile = configos.getenv('HOME')+os.sep+'.bohrium'+os.sep+'config.ini'
+	conffile = os.getenv('HOME')+os.sep+'.bohrium'+os.sep+'config.ini'
 	confparser = SafeConfigParser()     # Parser to modify the Bohrium configuration file.
 	confparser.read(conffile)             # Read current configuration
 	confparser.set("node", "children", "simple")  
