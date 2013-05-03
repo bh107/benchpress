8555bdea47173dc706d6f8931e6fe36e83e1899d
Mads R. B. Kristensen <madsbk@gmail.com>
2013-05-03 12:49:16 +0200

    The VE selecter is obsolete

--- select_ve.py
+++
@@ -1,9 +0,0 @@
-from ConfigParser import SafeConfigParser
-import os
-
-if __name__ == "__main__":
-	conffile = os.getenv('HOME')+os.sep+'.bohrium'+os.sep+'config.ini'
-	confparser = SafeConfigParser()     # Parser to modify the Bohrium configuration file.
-	confparser.read(conffile)             # Read current configuration
-	confparser.set("node", "children", "simple")  
-	confparser.write(open(conffile, 'wb'))
