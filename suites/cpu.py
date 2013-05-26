feb5adb5af0601e3602ffdeb23e1f927bc428769
Simon A. F. Lund <safl@safl.dk>
2013-05-26 19:45:49 +0200

    Added a suite for CPU.

---
+++ suites/cpu.py
@@ -0,0 +1,50 @@
+from default import *
+
+# Bridges  with various parameter setups
+# (alias, cmd (relative to the root of bohrium), env-vars)
+bridges = [
+    ('bohrium-numpy', 'python benchmark/Python/{script}.py {args} --bohrium=True', None),
+]
+
+# Managers above the node-vem with various parameter setups.
+# NB: the node-vem is hardcoded, the managers here will have the
+#     node-vem as child unless it is the node-vem itself
+# (alias, manager, cmd (relative to the root of bohrium), env-vars)
+managers = [
+    ('node',  'node', '',  None),
+]
+
+# Scripts and their arguments
+# (alias, script, arguments)
+scripts   = [
+    ('Black Scholes',        'black_scholes',  '--size=1000000*10'),
+    ('Jacobi Stencil',       'jacobi_stencil', '--size=20000*1000*10'),
+    ('KNN',                  'knn',            '--size=3000000*10*3'),
+    ('Lattice Boltzmann 2D', 'lattice_boltzmann_D2Q9',  '--size=1000*1000*10'),
+    ('Lattice Boltzmann 3D', 'lbm.3d',                  '--size=100*100*100*10'),
+    ('Monte Carlo PI',       'mc',             '--size=1000000*100'),
+    ('MXMUL',                'mxmul',             '--size=1000'),
+    ('Shallow Water',        'shallow_water',  '--size=1000*1000*10'),
+    ('NBody',                'nbody',          '--size=300*300'),
+    ('SoR',                  'sor',          '--size=5000*5000*10'),
+]
+
+# Engines with various parameter setups
+# (alias, engine, env-vars)
+engines = [
+    ('naive',       'naive',    None),
+    ('simple',      'simple',   None),
+    ('dynamite',    'dynamite', None)
+]
+
+# A suite example
+# Note that 'engines' and 'managers' may be undefined, in which case they are ignored
+suite = {
+    'bridges':   bridges,
+    'managers':  managers,
+    'engines':   engines,
+    'scripts':   scripts,
+}
+
+suites = [suite]
+
