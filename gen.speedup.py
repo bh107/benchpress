ea55167172d84db3441385abae22037dbd4db3d8
Simon A. F. Lund <safl@safl.dk>
2013-05-29 00:14:43 +0200

    Graphs: Added a graph generating script for comparing bridges/engines.

--- gen.speedup.py
+++ gen.speedup.py
@@ -13,11 +13,24 @@ import os
 import re
 
 colors = [
-    "#1B9E77", "#D95F02", "#7570B3",
-    "#66A61E", "#E7298A", "#E6AB02",
+    "#B3E2CD", "#FDCDAC", "#CBD5E8",
+    "#F4CAE4", "#E6F5C9", "#FFF2AE",
+    "#F1E2CC", "#CCCCCC",
+    "#B3E2CD", "#FDCDAC", "#CBD5E8",
+    "#F4CAE4", "#E6F5C9", "#FFF2AE",
+    "#F1E2CC", "#CCCCCC",
+    "#B3E2CD", "#FDCDAC", "#CBD5E8",
+    "#F4CAE4", "#E6F5C9", "#FFF2AE",
+    "#F1E2CC", "#CCCCCC",
 ]
 
-hatches = ["\\", "+", "o", "/", "-", "0"]
+hatches = [
+    "\\", "+", "o", "/", "-", "O",
+    "\\", "+", "o", "/", "-", "O",
+    "\\", "+", "o", "/", "-", "O",
+    "\\", "+", "o", "/", "-", "O",
+    "\\", "+", "o", "/", "-", "O",
+]
 
 def stats(samples):
     """Returns: (avg, lowest, highest, deviation)"""
@@ -45,79 +58,17 @@ def lintify(text):
 
     return ints
 
-def parse_perf( perf_tl ):
+def parse_results(results_fn):
     """Parses a list of 'perf' output into list of dicts with counter as key."""
-
-    perfs = []
-    for perf_t in perf_tl:
-        counters = {}
-        for m in re.finditer('\s+([0-9,\.]+)\s(\w+-(?:[-\w]+))\s+#', perf_t):
-            counters[m.group(2)] = int(m.group(1).replace(',','').replace('.',''))
-        perfs.append(counters)
-   
-    return perfs
-
-def render_abs(script, labels, values, output="/tmp", file_formats=['pdf']):
-    pos = arange(len(values))
-
-
-    clf()                       # Essential! Othervise the plots will be f**d up!
-    figure(1)
-
-    for p in pos:
-        bar(
-            pos[p],
-            values[p], 
-            align='center',
-            color=colors[p],
-            hatch=hatches[p]
-        )
-
-    ylabel('Time in seconds')
-
-    rotation = 'horizontal'     # Assume that there is not enough room vertically
-    if len(values)> 4:
-        rotation = 'vertical'
-
-    def hahaha( text ):
-        return re.search('\d+$', text).group(0)
-
-    #lbl = [hahaha(l) for l in labels if '_' in l]
-    lbl = labels
-
-    xticks(pos, lbl, rotation=rotation)
-    xlabel('Engine')
-    title(script)
-
-    gca().yaxis.grid(True)
-    gca().xaxis.grid(False)
-    gca().set_axisbelow(True)
-                                # Output them
-    fn = output +os.sep+ script.lower() +'_runtime'
-    dname = os.path.dirname(fn)
-    bname = re.sub('\W', '_', os.path.basename(fn))
-    fn = dname +os.sep+ bname
-
-    try:                        # Create output directory
-        os.makedirs(output)
-    except:
-        pass
-
-    for ff in file_formats:     # Create the physical files
-        savefig("%s.%s" % ( fn, ff ))
-
-    show()
-
-def main(results, output, file_formats):
 
     # Parse results file into something for the render
     res = []
-    with(open(results)) as fd:
+    with(open(results_fn)) as fd:
         for run in json.load(fd)['runs']:
             if (run['bridge_alias'] == "bohrium-numpy"):
                 tmp = (run['script_alias'], 
                        'bh', 
-                       run['engine'],
+                       run['engine_alias'],
                        stats(run['times'])[0]
                 )
             else:
@@ -143,13 +94,137 @@ def main(results, output, file_formats):
             labels[script] = [label]
             values[script] = [value]
 
+    return (labels, values)
+
+class Graph:
+    """
+    Baseclass for rendering Matplotlib graphs.
+    Does all alot of the annoying work, just override the render(...) method,
+    and you are good to go!
+    """
+
+    def __init__(
+        self,
+        output="/tmp",
+        file_formats=["pdf"],
+        graph_title="Unknown Graph",
+        xaxis_label="Bridge/Engine",
+        yaxis_label="Time in Seconds"
+    ):
+        self.graph_title    = graph_title
+        self.xaxis_label    = xaxis_label
+        self.yaxis_label    = yaxis_label
+        self.output         = output
+        self.file_formats   = file_formats
+
+    def prep(self):
+        clf()                       # Essential! Othervise the plots will be f**d up!
+        figure(1)
+
+        gca().yaxis.grid(True)      # Makes it easier to compare bars
+        gca().xaxis.grid(False)
+        gca().set_axisbelow(True)
+
+        title(self.graph_title)     # Title and axis labels
+        ylabel(self.yaxis_label)    # Label on the y-axis
+        xlabel(self.xaxis_label)    # Label on the x-axis
+
+    def render(self):
+        raise Exception('YOU ARE DOING IT WRONG!')
+
+    def to_file(self, text):
+        fn = self.output +os.sep+ text.lower() +'_runtime'
+        dname = os.path.dirname(fn)
+        bname = re.sub('\W', '_', os.path.basename(fn))
+        fn = dname +os.sep+ bname
+
+        try:                            # Create output directory
+            os.makedirs(self.output)
+        except:
+            pass
+
+        for ff in self.file_formats:    # Create the physical files
+            savefig("%s.%s" % (fn, ff))
+
+        show()
+
+class Absolute(Graph):
+    """
+    Create a graph that shows elapsed time on the y-axis and engines on 
+    along the x-axis using "bridge_alias/engine_alias" as labels.
+    """
+
+    def render(self, script, labels, values):
+
+        self.prep()                         # Prep it / clear the drawing board
+
+        rotation = 'horizontal'             # Assume that there is not enough room vertically
+        if len(values)> 4:                  # so change orientation of labels
+            rotation = 'vertical'
+
+        pos = arange(len(values))           # Output values as bars
+        for p in pos:
+            bar(
+                pos[p],
+                values[p], 
+                align='center',
+                color=colors[p],
+                hatch=hatches[p]
+            )
+
+        xticks(pos, labels, rotation=rotation)  # Output labels
+
+        self.to_file(script)                # Spit them out to file
+
+class Speedup(Absolute):
+    """
+    Create a graph that shows speedup on the y-axis and engines on 
+    along the x-axis using "bridge_alias/engine_alias" as labels.
+    "Baseline" is a string on the form: "bridge_alias/engine_alias".
+    """
+
+    def render(self, baseline, script, labels, values):
+
+                                            # Get index of baseline label
+        bl_index = [c for c, lable in enumerate(labels) if lable == baseline]
+        if not bl_index:
+            raise Exception('Baseline label was not found!')
+        bl_index = bl_index[0]
+
+        bl_label = labels.pop(bl_index)     # Remove baseline label from labels
+        bl_value = values.pop(bl_index)     # Remove baseline value from values
+
+                                            # Turn elapsed time into speedup in
+                                            # relation to bl_value
+        values = [bl_value/v for v in values]
+
+        values.insert(0, 1.0)               # Prepend baseline "value"
+        labels.insert(0, bl_label)          # Prepend baseline label
+
+        Absolute.render(self, script, labels, values)
+
+def main(results, output, file_formats):
+
+    labels, values = parse_results(results)
+
     for script in labels:
-        render_abs(
+        #Absolute(output, file_formats).render(
+        #    script,
+        #    labels[script],
+        #    values[script]
+        #)
+        bl_label = "bh/dyn_01"
+        bl_label = "py/numpy"
+        Speedup(
+            output,
+            file_formats,
+            script,
+            yaxis_label='Speedup in relation to "%s"' % bl_label
+        ).render(
+            bl_label,
             script,
             labels[script],
-            values[script],
-            output,
-            file_formats
+            values[script]
         )
 
     return [(True, "")]
