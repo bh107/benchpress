b1259b6ecf17331881c495e0ae9755652c48e341
Simon A. F. Lund <safl@safl.dk>
2013-06-25 21:27:00 +0200

    ...

--- suites/cpu.py
+++ suites/cpu.py
@@ -19,22 +19,31 @@ managers = [
 scripts   = [
     ('Black Scholes',        'black_scholes',  '--size=1000000*10'),
     ('Jacobi Stencil',       'jacobi_stencil', '--size=20000*1000*10'),
-    ('KNN',                  'knn',            '--size=3000000*10*3'),
-    ('Lattice Boltzmann 2D', 'lattice_boltzmann_D2Q9',  '--size=1000*1000*10'),
-    ('Lattice Boltzmann 3D', 'lbm.3d',                  '--size=100*100*100*10'),
-    ('Monte Carlo PI',       'mc',             '--size=1000000*100'),
-    ('MXMUL',                'mxmul',             '--size=1000'),
-    ('Shallow Water',        'shallow_water',  '--size=1000*1000*10'),
-    ('NBody',                'nbody',          '--size=300*300'),
-    ('SoR',                  'sor',          '--size=5000*5000*10'),
+    ('KNN',                  'knn',            '--size=2000000*10*3'),
+#    ('Lattice Boltzmann 2D', 'lattice_boltzmann_D2Q9',  '--size=1000*1000*10'),
+#    ('Lattice Boltzmann 3D', 'lbm.3d',          '--size=100*100*100*10'),
+    ('Monte Carlo PI',       'mc',              '--size=1000000*100'),
+#    ('MXMUL',                'mxmul',           '--size=1000'),
+    ('NBody',                'nbody',           '--size=300*300'),
+    ('Shallow Water',        'shallow_water',   '--size=1000*1000*10'),
+#    ('SoR',                  'sor',             '--size=5000*5000*10'),
 ]
 
 # Engines with various parameter setups
 # (alias, engine, env-vars)
 engines = [
-    ('naive',       'naive',    None),
-    ('simple',      'simple',   None),
-    ('dynamite',    'dynamite', None)
+    ('naive',       'cpu', {'BH_CORE_VCACHE_SIZE': '0',
+                            'BH_VE_CPU_TRAVERSAL': 'naive'}),
+    ('naive+vc',    'cpu', {'BH_CORE_VCACHE_SIZE': '10',
+                            'BH_VE_CPU_TRAVERSAL': 'naive'}),
+    ('fl',          'cpu', {'BH_CORE_VCACHE_SIZE' : '0',
+                            'BH_VE_CPUT_TRAVERSAL': 'fruit_loops'}),
+    ('fl+vc',       'cpu', {'BH_CORE_VCACHE_SIZE' : '10',
+                            'BH_VE_CPUT_TRAVERSAL': 'fruit_loops'}),
+    ('tiling',      'tiling',   {'BH_CORE_VCACHE_SIZE': '0'}),
+    ('tiling+vc',   'tiling',   {'BH_CORE_VCACHE_SIZE': '10'}),
+    ('mcore',       'mcore',    {'BH_CORE_VCACHE_SIZE': '0'}),
+    ('mcore+vc',    'mcore',    {'BH_CORE_VCACHE_SIZE': '10'})
 ]
 
 # A suite example
