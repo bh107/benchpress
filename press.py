9660d9e986409aae9d2724aa92ae3332b3cbabec
Simon A. F. Lund <safl@safl.dk>
2013-08-27 18:39:50 +0200

    Added printing of name of the result-file in slurm gather.

--- press.py
+++ press.py
@@ -311,7 +311,8 @@ def slurm_gather( result_file ):
                     print "ERR job %d: %s"%(job['id'], fd.read())
             except IOError:
                 print "WARNING: couldn't find job file '%s'"%job['out']
-    print "Result-file: %s" % result_file
+
+    print "Result-file: %s" % result_file.name
 
 def gen_jobs(result_file, config, src_root, output, suite, benchmarks, use_perf):
     """Generates benchmark jobs based on the benchmark suites"""
