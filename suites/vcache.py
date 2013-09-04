bd5d9e521146c385e49d367e7d27c3bcdd4dd830
Simon A. F. Lund <safl@safl.dk>
2013-09-05 00:11:16 +0200

    Stuff...

--- suites/vcache.py
+++ suites/vcache.py
@@ -1,107 +1,22 @@
 from default import *
 
-scholes = [
-#    ('Black Scholes',        'black_scholes',   '--size=250000*5'),
-#    ('Black Scholes',        'black_scholes',   '--size=500000*5'),
-#    ('Black Scholes',        'black_scholes',   '--size=1000000*5'),
-#    ('Black Scholes',        'black_scholes',   '--size=2000000*5'),
-#    ('Black Scholes',        'black_scholes',   '--size=4000000*5'),
-    ('Black Scholes',        'black_scholes',   '--size=8000000*5'),
+scripts = [
+    ('Black Scholes',   'black_scholes',    '--size=8000000*5'),
+    ('Jacobi Stencil',  'jacobi_stencil',   '--size=8000*8000*3'),
+    ('KNN',             'knn',              '--size=2000000*10*3'),
+    ('Monte Carlo PI',  'mc',               '--size=20000000*10'),
+    ('NBody',           'nbody',            '--size=3000*1'),
+    ('Shallow Water',   'shallow_water',    '--size=3000*3000*5'),
+    ('Swaption',        'LMM_swaption_vec', '--size=5000'),
+    ('Bolzmann D2Q9',   'lattice_boltzmann_D2Q9', '--size=1400*1400*2'),
+    ('Bolzmann 3D',     'lbm.3d',           '--size=120*100*100*5'),
+    ('Matrix Mul',      'mxmul',            '--size=800')        
+    ('SOR',             'sor',              '--size=4000*4000*5')
+    ('Wire World',      'wireworldnumpy',   '--size=5000*5000*5')
+    ('FFT',             'fftex',            '--size=18')
+    ('LU Factor.',      'lu',               '--size=6000*1')
+    ('Cloth',           'cloth',            '--size=3000*3000*1')
 ]
-
-jacobi = [
-#    ('Jacobi Stencil',       'jacobi_stencil', '--size=313*4000*10'),
-#    ('Jacobi Stencil',       'jacobi_stencil', '--size=625*4000*10'),
-#    ('Jacobi Stencil',       'jacobi_stencil', '--size=1250*4000*10'),
-#    ('Jacobi Stencil',       'jacobi_stencil', '--size=2500*4000*10'),
-#    ('Jacobi Stencil',       'jacobi_stencil', '--size=5000*4000*10'),
-    ('Jacobi Stencil',       'jacobi_stencil', '--size=8000*8000*3'),
-]
-
-knn = [
-#    ('KNN', 'knn', '--size=62500*10*3'),
-#    ('KNN', 'knn', '--size=125000*10*3'),
-#    ('KNN', 'knn', '--size=250000*10*3'),
-#    ('KNN', 'knn', '--size=500000*10*3'),
-#    ('KNN', 'knn', '--size=1000000*10*3'),
-    ('KNN', 'knn', '--size=2000000*10*3'),
-]
-
-mc = [
-#    ('Monte Carlo PI', 'mc', '--size=625000*10'),
-#    ('Monte Carlo PI', 'mc', '--size=1250000*10'),
-#    ('Monte Carlo PI', 'mc', '--size=2500000*10'),
-#    ('Monte Carlo PI', 'mc', '--size=5000000*10'),
-#    ('Monte Carlo PI', 'mc', '--size=10000000*10'),
-    ('Monte Carlo PI', 'mc', '--size=20000000*10'),
-]
-
-nbody = [
-#    ('NBody', 'nbody', '--size=125*10'),
-#    ('NBody', 'nbody', '--size=250*10'),
-#    ('NBody', 'nbody', '--size=500*10'),
-#    ('NBody', 'nbody', '--size=1000*10'),
-    ('NBody', 'nbody', '--size=3000*1'),
-#    ('NBody', 'nbody', '--size=4000*10')
-]
-
-shallow = [
-#    ('Shallow Water',        'shallow_water',   '--size=94*94*5'),
-#    ('Shallow Water',        'shallow_water',   '--size=188*188*5'),
-#    ('Shallow Water',        'shallow_water',   '--size=375*375*5'),
-#    ('Shallow Water',        'shallow_water',   '--size=750*750*5'),
-#    ('Shallow Water',        'shallow_water',   '--size=1500*1500*5'),
-    ('Shallow Water',        'shallow_water',   '--size=3000*3000*5'),
-]
-
-swaption = [
-#    ('Swaption', 'LMM_swaption_vec', '--size=1250'),
-#    ('Swaption', 'LMM_swaption_vec', '--size=2500'),
-#    ('Swaption', 'LMM_swaption_vec', '--size=5000'),
-#    ('Swaption', 'LMM_swaption_vec', '--size=10000'),
-#    ('Swaption', 'LMM_swaption_vec', '--size=20000'),
-    ('Swaption', 'LMM_swaption_vec', '--size=5000'),
-]
-
-bolz = [
-#    ('Bolzmann D2Q9', 'lattice_boltzmann_D2Q9', '--size=94*94*5'),
-#    ('Bolzmann D2Q9', 'lattice_boltzmann_D2Q9', '--size=188*188*5'),
-#    ('Bolzmann D2Q9', 'lattice_boltzmann_D2Q9', '--size=375*375*5'),
-#    ('Bolzmann D2Q9', 'lattice_boltzmann_D2Q9', '--size=750*750*5'),
-#    ('Bolzmann D2Q9', 'lattice_boltzmann_D2Q9', '--size=1500*1500*5'),
-    ('Bolzmann D2Q9', 'lattice_boltzmann_D2Q9', '--size=1400*1400*2'),
-]
-
-bolz3d = [
-    ('Bolzmann 3D', 'lbm.3d', '--size=120*100*100*5'),
-]
-
-mxmul = [
-    ('Matrix Mul', 'mxmul', '--size=800')        
-]
-
-sor = [
-    ('SOR', 'sor', '--size=4000*4000*5')
-]
-
-wworld = [
-    ('Wire World', 'wireworldnumpy', '--size=5000*5000*5')
-]
-
-
-fft = [
-    ('FFT', 'fftex', '--size=18')
-]
-
-lu = [
-    ('LU Factor.', 'lu', '--size=6000*1')
-]
-
-cloth = [
-    ('Cloth', 'cloth', '--size=3000*3000*1')
-]
-
-scripts = scholes + jacobi + knn + mc + nbody + shallow + swaption + bolz + bolz3d + mxmul + sor + wworld + lu +fft + cloth
 
 numpy = {
     'bridges':  [
@@ -113,8 +28,6 @@ numpy = {
             {'VCACHE_LINES': "10",'VCACHE_BYTES': "536870912"} ),
         ('NumPy/VCache_1G', 'python benchmark/Python/{script}.py {args}',
             {'VCACHE_LINES': "10",'VCACHE_BYTES': "1048576000"} ),
-#        ('NumPy/VCache_2G', 'python benchmark/Python/{script}.py {args}',
-#            {'VCACHE_LINES': "20",'VCACHE_BYTES': "2147483648"} ),
 
     ],
     'scripts':  scripts,
