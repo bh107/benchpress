ded6abc3b341e67f13a38b007efbaad49640270c
Simon A. F. Lund <safl@safl.dk>
2013-09-05 15:29:23 +0200

    ...

--- suites/vcache.py
+++ suites/vcache.py
@@ -1,21 +1,21 @@
 from default import *
 
 scripts = [
+    ('LU Factor.',      'lu',               '--size=500*499'),
+    ('Jacobi Stencil',  'jacobi_stencil',   '--size=10000*4000*10'),
+    ('Shallow Water',   'shallow_water',    '--size=3000*3000*5'),
+    ('Cloth',           'cloth',            '--size=3000*3000*1'),
+    ('SOR',             'sor',              '--size=4000*4000*5'),
     ('Black Scholes',   'black_scholes',    '--size=8000000*5'),
-    ('Jacobi Stencil',  'jacobi_stencil',   '--size=8000*8000*3'),
     ('KNN',             'knn',              '--size=2000000*10*3'),
     ('Monte Carlo PI',  'mc',               '--size=20000000*10'),
     ('NBody',           'nbody',            '--size=3000*1'),
-    ('Shallow Water',   'shallow_water',    '--size=3000*3000*5'),
-    ('Swaption',        'LMM_swaption_vec', '--size=5000'),
-    ('Bolzmann D2Q9',   'lattice_boltzmann_D2Q9', '--size=1400*1400*2'),
+    ('Swaption',        'LMM_swaption_vec', '--size=1000'),
+    ('Bolzmann D2Q9',   'lattice_boltzmann_D2Q9', '--size=800*800*5'),
     ('Bolzmann 3D',     'lbm.3d',           '--size=120*100*100*5'),
     ('Matrix Mul',      'mxmul',            '--size=800'),
-    ('SOR',             'sor',              '--size=4000*4000*5'),
     ('Wire World',      'wireworldnumpy',   '--size=5000*5000*5'),
     ('FFT',             'fftex',            '--size=18'),
-    ('LU Factor.',      'lu',               '--size=6000*1'),
-    ('Cloth',           'cloth',            '--size=3000*3000*1')
 ]
 
 numpy = {
