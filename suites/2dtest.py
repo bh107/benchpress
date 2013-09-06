e40c05c1691cfe5892c6612652991f64a2b83271
Troels Blum <troels@blum.dk>
2013-09-06 12:13:01 +0200

    2D structure test for GPU

---
+++ suites/2dtest.py
@@ -0,0 +1,42 @@
+# Bridges  with various parameter setups
+# (alias, cmd (relative to the root of bohrium), env-vars)
+bohrium = [
+    ('bohrium', 'python benchmark/Python/{script}.py {args} --bohrium=True', None)
+]
+numpy = [
+    ('numpy', 'python benchmark/Python/{script}.py {args} --bohrium=False', None)
+]
+
+# Engines with various parameter setups
+# (alias, engine, env-vars)
+engines = [
+    ('GPU',     'gpu',      None),
+]
+
+# Scripts and their arguments
+# (alias, script, arguments)
+scripts   = [
+    ('SOR f32  2k x  8k',         'sor',            '--size=2046*8190*100 --dtype=float32'),
+    ('SOR f32  4k x  4k',         'sor',            '--size=4096*4096*100 --dtype=float32'),
+    ('SOR f32  8k x  4k',         'sor',            '--size=8190*2046*100 --dtype=float32'),
+
+    ('SOR f64  2k x  8k',         'sor',            '--size=2046*8190*100 --dtype=float64'),
+    ('SOR f64  4k x  4k',         'sor',            '--size=4096*4096*100 --dtype=float64'),
+    ('SOR f64  8k x  4k',         'sor',            '--size=8190*2046*100 --dtype=float64'),
+
+    ('Shallow Water f32 2k x 8k', 'shallow_water',  '--size=2048*8192*100 --dtype=float32'),
+    ('Shallow Water f32 4k x 4k', 'shallow_water',  '--size=4096*4096*100 --dtype=float32'),
+    ('Shallow Water f32 8k x 2k', 'shallow_water',  '--size=8192*2048*100 --dtype=float32'),
+
+    ('Shallow Water f64 2k x 8k', 'shallow_water',  '--size=2048*8192*100 --dtype=float64'),
+    ('Shallow Water f64 4k x 4k', 'shallow_water',  '--size=4096*4096*100 --dtype=float64'),
+    ('Shallow Water f64 8k x 2k', 'shallow_water',  '--size=8192*2048*100 --dtype=float64'),
+
+
+]
+
+suites = [
+    { 'bridges': bohrium
+      ,'engines':  engines
+      ,'scripts':  scripts}
+    ]
