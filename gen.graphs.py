d963ef710a886860b5c0a30875dc5f75da145d96
Simon A. F. Lund <safl@safl.dk>
2013-09-25 10:21:01 +0200

    Grapher: Added scalability graph for multi-core.

--- gen.graphs.py
+++ gen.graphs.py
@@ -102,17 +102,31 @@ class Scale(Graph):
 
         self.prep()                         # Prep it / clear the drawing board
 
-
         values = []
         for label, samples in data:
             if not 'omp' in label:
                 continue
 
             lbl, threads = label.split('omp')
-            values.append((int(threads), samples['elapsed']['avg']))
+            values.append((int(threads), samples['elapsed']['avg'].pop()))
         values.sort()
 
-        
+        sizes   = []
+        speedup = []
+        for nthreads, times in values:
+            sizes.append(nthreads)
+            speedup.append(times)
+
+        linear = [2**x for x in range(0, len(sizes))]
+        xscale("log")   # Scale with a neat border
+        yscale("log")
+        xticks(linear, linear)
+        xlim(xmin=min(linear)*0.85, xmax=max(linear)*1.25)
+        yticks(linear, linear)
+        ylim(ymin=min(linear)*0.85, ymax=max(linear)*1.25)
+
+        plot(linear, linear)
+        plot(linear, speedup)
 
         self.to_file(script)                # Spit them out to file
 
@@ -246,7 +260,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         '--formats',
-        default=['pdf'],
+        default=['png'],
         nargs='+',
         help="Output file-format(s) of the generated graph(s).",
         choices=[ff for ff in formats]
