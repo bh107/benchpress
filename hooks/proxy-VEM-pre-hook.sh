91d933c24150a9381e3235b66f71647eee20d9d8
Mads R. B. Kristensen <madsbk@gmail.com>
2014-03-03 19:01:07 +0100

    updated the proxy-vem suite

--- hooks/proxy-VEM-pre-hook.sh
+++ hooks/proxy-VEM-pre-hook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 1
 export LD_LIBRARY_PATH=~/.local
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=8
 echo mpiexec -ppn 4 -np 1 ~/.local/bh_proxy_client -a 10.10.0.100 -p 4200 : -np 31 ~/.local/bh_vem_cluster_slave
-mpiexec -ppn 4 -np 1 ~/.local/bh_proxy_client -a 10.10.0.100 -p 4200 : -np 7 ~/.local/bh_vem_cluster_slave
+mpiexec -ppn 4 -np 1 ~/.local/bh_proxy_client -a 10.10.0.100 -p 4200 : -np 31 ~/.local/bh_vem_cluster_slave
--- suites/proxy-VEM.py
+++ suites/proxy-VEM.py
@@ -1,14 +1,26 @@
 managers = [('proxy', 'proxy', '{bridge}', None)]
 
-engines = [('cpu',  'cpu',   None)]
+engines = [\
+           ('sleep    0ms', 'cpu',  {'BH_VEM_PROXY_SLEEP':0}),
+           ('sleep   10ms', 'cpu',  {'BH_VEM_PROXY_SLEEP':10}),
+           ('sleep  100ms', 'cpu',  {'BH_VEM_PROXY_SLEEP':100}),
+           ('sleep  200ms', 'cpu',  {'BH_VEM_PROXY_SLEEP':200}),
+           ('sleep  500ms', 'cpu',  {'BH_VEM_PROXY_SLEEP':500}),
+           ('sleep 1000ms', 'cpu',  {'BH_VEM_PROXY_SLEEP':1000}),
+]
 
 python_script = [\
-#    		 ('N-body 5k',        'nbody',        '--size=150*10'),
-#           	 ('N-body 20k',        'nbody',        '--size=200*10'),
-                 ('Shallow Water 10k', 'shallow_water','--size=10000*10000*10'),
-#                 ('Shallow Water 25k', 'shallow_water','--size=250*250*10'),
-#                 ('Black Scholes 10m', 'black_scholes','--size=10000*10'),
-#                 ('Black Scholes 100m','black_scholes','--size=10000000*10')
+    		 ('N-body  5k',        'nbody',          '--size=5000*10'),
+    		 ('N-body 10k',        'nbody',          '--size=10000*10'),
+    		 ('N-body 15k',        'nbody',          '--size=15000*10'),
+    		 ('Heat Equation  5k', 'heat_equation',  '--size=5000*5000*10'),
+    		 ('Heat Equation 10k', 'heat_equation',  '--size=10000*10000*10'),
+    		 ('Heat Equation 20k', 'heat_equation',  '--size=20000*20000*10'),
+                 ('Shallow Water  5k', 'shallow_water',  '--size=5000*5000*10'),
+                 ('Shallow Water 10k', 'shallow_water',  '--size=10000*10000*10'),
+                 ('Shallow Water 20k', 'shallow_water',  '--size=20000*20000*10'),
+                 ('Black Scholes  10m', 'black_scholes',  '--size=10000000*10'),
+                 ('Black Scholes 100m', 'black_scholes',  '--size=100000000*10'),
 ]
 
 python = {
