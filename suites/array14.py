7f4e77de3509d139f2cd5ec9fcba5615b9d6befa
Simon A. F. Lund <safl@safl.dk>
2014-04-07 00:35:29 +0200

    Added suite for ARRAY14 paper.

---
+++ suites/array14.py
@@ -0,0 +1,27 @@
+from default import *
+
+scripts = [
+    ('Convolve',    'convolve',     '--size=25'),
+    ('Wireworld',   'wireworld',    '--size=1000*10'),
+    ('27-Point',    'point27',      '--size=400*10'),
+]
+
+managers= [('node', 'node', '', None)]
+
+numpy = {
+    'bridges':  [('NumPy', 'python benchmark/Python/{script}.py {args} --bohrium=False', None)],
+    'scripts':  scripts,
+}
+
+bohrium = {
+    'bridges':  [('Bohrium', 'python benchmark/Python/{script}.py {args} --bohrium=True', None)],
+    'managers': [('node',   'node', '',  None) ],
+    'engines':  [('static', 'cpu',  None)],
+    'scripts':  scripts
+}
+
+suites = [
+    bohrium,
+    numpy
+]
+
