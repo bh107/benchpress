2a9d7111069f9c4611faf0e46c1ff0325e3d60ef
Mads R. B. Kristensen <madsbk@gmail.com>
2014-09-19 10:23:48 +0200

    daily_benchmark: added some more benchmarks

--- suites/daily_benchmark.py
+++ suites/daily_benchmark.py
@@ -1,9 +1,19 @@
 from default import *
 
 scripts   = [
-    ('Jacobi Stencil', 'jacobi_stencil', '--size=100*100*10'),
-    ('Shallow Water',  'shallow_water',  '--size=100*100*10'),
-    ('N-Body',         'nbody',          '--size=100*10'),
+    ('Jacobi Stencil',        'jacobi_stencil',        '--size=100*100*10'),
+    ('Shallow Water',         'shallow_water',         '--size=100*100*10'),
+    ('Heat Equation',         'heat_equation',         '--size=100*100*10'),
+    ('N-Body',                'nbody',                 '--size=100*10'),
+    ('Snakes and Ladders',    'snakes_and_ladders',    '--size=100*10'),
+    ('Wire World',            'wireworld',             '--size=10*10'),
+    ('Monte Carlo Pi',        'mc',                    '--size=100*10'),
+    ('Lattice Boltzmann D2Q9','lattice_boltzmann_D2Q9','--size=100*100*10'),
+    ('Gauss Elimination',     'gauss',                 '--size=100'),
+    ('Convolution 2D',        'convolve_2d',           '--size=5'),
+    ('Convolution 3D',        'convolve_3d',           '--size=5'),
+    ('Matrix Multiplication', 'mxmul',                 '--size=100'),
+    ('LU Factorization',      'lu',                    '--size=100'),
 ]
 
 bohrium = {
@@ -14,10 +24,10 @@ bohrium = {
 }
 
 numpy = {
-    'bridges': [('Bohrium', 'python benchmark/Python/{script}.py {args} --bohrium=True', None)],
+    'bridges': [('NumPy', 'python benchmark/Python/{script}.py {args} --bohrium=False', None)],
     'engines': [('CPU', 'cpu', None)],
     'scripts': scripts,
 }
 
-suites = [numpy, python]
+suites = [numpy, bohrium]
 
