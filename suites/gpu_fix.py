3031635c1b76ef551570c37c3e71a06fc413208a
Troels Blum <troels@blum.dk>
2014-11-26 14:52:55 +0100

    Suits for GPU dyn/fix kernel paper

--- suites/gpu_fix.py
+++ suites/gpu_fix.py
@@ -17,14 +17,10 @@ scripts   = [
     ('2D f64',    'ndstencil', '--size=24*4000*2 --dtype=float64'),
     ('3D f64',    'ndstencil', '--size=24*2000*3 --dtype=float64'),
     ('4D f64',    'ndstencil', '--size=24*1000*4 --dtype=float64'),
-    ('Gauss f64', 'gauss',     '--size=9000 --dtype=float64'),
-    ('LU f64',    'lu',        '--size=9000 --dtype=float64'),
     ('1D f32',    'ndstencil', '--size=25*8000*1 --dtype=float32'),
     ('2D f32',    'ndstencil', '--size=25*4000*2 --dtype=float32'),
     ('3D f32',    'ndstencil', '--size=25*2000*3 --dtype=float32'),
     ('4D f32',    'ndstencil', '--size=25*1000*4 --dtype=float32'),
-    ('Gauss f32', 'gauss',     '--size=12000 --dtype=float32'),
-    ('LU f32',    'lu',        '--size=12000 --dtype=float32'),
 
 ]
 
---
+++ suites/gpu_fix_com.py
@@ -0,0 +1,31 @@
+# Bridges  with various parameter setups
+# (alias, cmd (relative to the root of bohrium), env-vars)
+bohrium = [
+    ('bohrium', 'python benchmark/Python/{script}.py {args} --bohrium=True', None)
+]
+
+# Engines with various parameter setups
+# (alias, engine, env-vars)
+engines = [
+    ('GPU',     'gpu',      None),
+]
+
+# Scripts and their arguments
+# (alias, script, arguments)
+scripts   = [
+    ('Gauss f64 1i', 'gauss',     '--size=9000*1 --dtype=float64'),
+    ('LU f64 1i',    'lu',        '--size=9000*1 --dtype=float64'),
+    ('Gauss f32 1i', 'gauss',     '--size=12000*1 --dtype=float32'),
+    ('LU f32 1i',    'lu',        '--size=12000*1 --dtype=float32'),
+    ('Gauss f64 100i', 'gauss',     '--size=9000*100 --dtype=float64'),
+    ('LU f64 100i',    'lu',        '--size=9000*100 --dtype=float64'),
+    ('Gauss f32 100i', 'gauss',     '--size=12000*100 --dtype=float32'),
+    ('LU f32 100i',    'lu',        '--size=12000*100 --dtype=float32'),
+
+]
+
+suites = [
+    { 'bridges': bohrium
+      ,'engines':  engines
+      ,'scripts':  scripts},
+    ]
