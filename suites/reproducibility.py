92f1691e419ad08fd1ef3a27b3817c2ca385b4d8
Mads R. B. Kristensen <madsbk@gmail.com>
2015-03-26 15:59:48 +0100

    added some benchmarks to the reproducibility suite

--- suites/reproducibility.py
+++ suites/reproducibility.py
@@ -1,8 +1,10 @@
 scripts   = [
+    ('SOR',                   'sor',                   '--size=300*300*10'),
     ('Jacobi Stencil',        'jacobi_stencil',        '--size=300*300*10'),
     ('Shallow Water',         'shallow_water',         '--size=200*200*10'),
     ('Heat Equation',         'heat_equation',         '--size=300*300*100'),
     ('N-Body',                'nbody',                 '--size=100*100'),
+    ('N-Body Nice',           'nbody_nice',            '--size=500*100*10'),
     ('Black Scholes',         'black_scholes',         '--size=10000*100'),
     ('Gauss Elimination',     'gauss',                 '--size=500'),
     ('Matrix Multiplication', 'mxmul',                 '--size=500'),
