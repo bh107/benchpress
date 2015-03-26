bc93efd32e8971b2267c3001e83820d6c13644f3
Mads R. B. Kristensen <madsbk@gmail.com>
2015-03-26 15:39:21 +0100

    updated the reproducibility suite

--- suites/reproducibility.py
+++ suites/reproducibility.py
@@ -1,16 +1,12 @@
 scripts   = [
     ('Jacobi Stencil',        'jacobi_stencil',        '--size=300*300*10'),
     ('Shallow Water',         'shallow_water',         '--size=200*200*10'),
-    #    ('Heat Equation',         'heat_equation',         '--size=3000*3000*100'),
-    #    ('N-Body',                'nbody',                 '--size=1000*100'),
-    #    ('Wire World',            'wireworld',             '--size=100*100'),
-    #    ('Monte Carlo Pi',        'mc',                    '--size=10000000*100'),
-    #    ('Black Scholes',         'black_scholes',         '--size=1000000*100'),
-    #    ('Lattice Boltzmann D2Q9','lattice_boltzmann_D2Q9','--size=1000*1000*10'),
-    #    ('Gauss Elimination',     'gauss',                 '--size=1000'),
-    #    ('Convolution 3D',        'convolve_3d',           '--size=100'),
-    #    ('Matrix Multiplication', 'mxmul',                 '--size=1000'),
-    #    ('LU Factorization',      'lu',                    '--size=1000'),
+    ('Heat Equation',         'heat_equation',         '--size=300*300*100'),
+    ('N-Body',                'nbody',                 '--size=100*100'),
+    ('Black Scholes',         'black_scholes',         '--size=10000*100'),
+    ('Gauss Elimination',     'gauss',                 '--size=500'),
+    ('Matrix Multiplication', 'mxmul',                 '--size=500'),
+    ('LU Factorization',      'lu',                    '--size=500'),
 ]
 
 bohrium = {
