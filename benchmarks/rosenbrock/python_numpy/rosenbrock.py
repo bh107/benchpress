efbcb17f98f1fd31415a50307c0287f8b242f9b6
Simon A. F. Lund <safl@safl.dk>
2015-04-15 03:41:58 +0200

    Added rosenbrock benchmark.

---
+++ benchmarks/rosenbrock/python_numpy/rosenbrock.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+from benchpress import util
+import numpy as np
+
+def rosen(x):
+    return np.sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+
+def main():
+    B = util.Benchmark()                        # Initialize Benchpress
+    N, T = B.size                               # Grab command-line arguments
+    dataset = np.arange(N)/N                    # Create psuedo-data
+    B.start()                                   # Sample wall-clock start
+    for _ in xrange(0, T):                      # Do T trials of..
+        R = rosen(dataset)                      # ..executing rosenbrock.
+    B.stop()                                    # Sample wall-clock stop
+    B.pprint()                                  # Print elapsed wall-clock etc.
+    if B.verbose:                               # Print more, such as results
+        print(R)
+
+if __name__ == "__main__":
+    main()
