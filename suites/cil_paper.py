e94bdf7e76fd1f3ea0df5f77c6fc78d6ca95e114
Kenneth Skovhede <kenneth@hexad.dk>
2015-04-15 10:21:59 +0200

    Updated the cil_paper suite to the new format

--- suites/cil_paper.py
+++ suites/cil_paper.py
@@ -1,23 +1,45 @@
 from default import *
 
-scripts = [
-    ('Black Scholes',           'black_scholes',            '--size=5000000*10'),
+bh_stack_cpu_t32 = [
+    [('default',    'bridge',             None)],
+    [('node',       'node',               None)],
+    [('topo',       'topological',        None)],
+    [
+        ('cpu_t32', 'cpu',   {"OMP_NUM_THREADS": "32"}),
+        ('cpu_t16', 'cpu',   {"OMP_NUM_THREADS": "16"}),
+        ('cpu_t08', 'cpu',   {"OMP_NUM_THREADS": "8"}),
+        ('cpu_t04', 'cpu',   {"OMP_NUM_THREADS": "4"}),
+        ('cpu_t02', 'cpu',   {"OMP_NUM_THREADS": "2"}),
+        ('cpu_t01', 'cpu',   {"OMP_NUM_THREADS": "1"}),
+    ]
 ]
 
-references = {
-    'bridges':  [python_numpy, cil_managed, cil_unsafe],
-    'scripts':  scripts,
+scripts = [
+    ('Black Scholes',           'black_scholes',            '--size=3200000*36'),
+]
+
+
+bh_cpu_t32_suite = {
+    'scripts': scripts,
+    'launchers': [cil_bohrium],
+    'bohrium': bh_stack_cpu_t32
 }
 
-bohrium = {
-    'bridges':  [cil_bohrium],
-    'managers': [('node',  'node', '',  None)],
-    'engines':  [('cpu', 'cpu', None)],
-    'scripts':  scripts
+reference_suite = {
+    'scripts': scripts,
+    'launchers': [python_numpy, cil_managed, cil_unsafe]
 }
+
+np_bh_ref_suite = {
+    'scripts': scripts,
+    'launchers': [python_bohrium],
+    'bohrium': bh_stack_cpu_t32    
+}
+
 
 suites = [
-    references,
-    bohrium
+    bh_cpu_t32_suite,
+    reference_suite,
+    np_bh_ref_suite
 ]
 
