fa8f87afc902c24fa75e7afb425f9541044c18d8
Simon A. F. Lund <safl@safl.dk>
2015-04-20 00:29:16 +0200

    ..

---
+++ suites/cpu/perftest_mini.py
@@ -0,0 +1,9 @@
+from benchpress.default import *
+
+suites = [{
+    "scripts": [('Leibnitz PI',   'leibnitz_pi',  '--size=1000')],
+    "launchers": [python_numpy],
+    "bohrium": bh_stack_none,
+    "use_slurm_default": False,
+    #"use_slurm_default": True,
+}]
