ed5353bddbc12871c27c0b809f643ef3a07aab64
Simon A. F. Lund <safl@safl.dk>
2015-04-21 11:08:55 +0200

    ...

--- suites/cpu/heat_equation.py
+++ suites/cpu/heat_equation.py
@@ -8,25 +8,29 @@ scripts = [
 bohrium = {
     'scripts': scripts,
     'launchers': [python_bohrium],
-    'bohrium': bh_stack_cpu_t32
+    'bohrium': bh_stack_cpu_t32,
+    "use_slurm_default": True,
 }
 
 omp = {
     'scripts': scripts,
     'launchers': [cpp11_omp, c99_omp],
-    'bohrium': stack_omp_t32
+    'bohrium': stack_omp_t32,
+    "use_slurm_default": True,
 }
 
 cseq = {
     'scripts': scripts,
     'launchers': [c99_seq],
-    'bohrium': bh_stack_none
+    'bohrium': bh_stack_none,
+    "use_slurm_default": True,
 }
 
 np = {
     'scripts': scripts,
     'launchers': [python_numpy],
-    'bohrium': bh_stack_none
+    'bohrium': bh_stack_none,
+    "use_slurm_default": True,
 }
 
 suites = [
