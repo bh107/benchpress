2cb2264948ea339ef553225af8774151c89ac5d0
Simon A. F. Lund <safl@safl.dk>
2015-04-23 13:06:09 +0200

    Slight modification to the Heat EQuation  / C++ / OMP.

--- benchmarks/heat_equation/cpp11_omp/src/heat_equation.cpp
+++ benchmarks/heat_equation/cpp11_omp/src/heat_equation.cpp
@@ -4,55 +4,16 @@
 #include <cmath>
 #include <bp_util.h>
 
+#define xdim 14000
+#define ydim 14000
+
 using namespace std;
 
-int main(int argc, char* argv[])
+void solve(auto grid, double epsilon, int max_iterations)
 {
-    bp_util_type bp = bp_util_create(argc, argv, 3);// Grab arguments
-    if (bp.args.has_error) {
-        return 1;
-    }
-    bp.timer_start();
-
-    const int width     = bp.args.sizes[0];
-    const int height    = bp.args.sizes[1];
-
-    const int ydim = 14000;
-    if (ydim != height) {
-        cout << "Multidimensional arrays in C11 does not support dynamic size, so it has to be: " << ydim << "." << endl;
-        return 0;
-    }
-
-    const int xdim = 14000;
-    if (xdim != width) {
-        cout << "Multidimensional arrays in C11 does not support dynamic size, so it has to be: " << xdim << "." << endl;
-        return 0;
-    }
-
-    const int max_iterations = bp.args.sizes[2];
-
-    double epsilon  = 0.005;
-    double delta    = epsilon+1.0;
-
-    auto grid = new double[ydim][xdim];
+    double delta = epsilon+1.0;
     auto temp = new double[ydim][xdim];
 
-    #pragma omp parallel for collapse(2)
-    for(int i=0; i<ydim; i++){      // Initialize the grid
-        for(int j=0;j<xdim;j++){
-            grid[i][j] = 0;
-        }
-    }
-    for(int i=0; i<ydim; i++){      // And borders
-        grid[i][0]      = -273.15;
-        grid[i][xdim-1] = -273.15;
-    }
-    for(int i=0; i<xdim; i++){
-        grid[0][i]      = -273.15;
-        grid[ydim-1][i] = 40.0;
-    }
-
-    bp.timer_start();
     auto iterations = 0;            // Compute the heat equation
     while(delta>epsilon) {
         ++iterations;
@@ -75,6 +36,51 @@ int main(int argc, char* argv[])
             break;
         }
     }
+}
+
+int main(int argc, char* argv[])
+{
+    bp_util_type bp = bp_util_create(argc, argv, 3);// Grab arguments
+    if (bp.args.has_error) {
+        return 1;
+    }
+    bp.timer_start();
+
+    const int width     = bp.args.sizes[0];
+    const int height    = bp.args.sizes[1];
+
+    if (ydim != height) {
+        cout << "Multidimensional arrays in C11 does not support dynamic size, so it has to be: " << ydim << "." << endl;
+        return 0;
+    }
+
+    if (xdim != width) {
+        cout << "Multidimensional arrays in C11 does not support dynamic size, so it has to be: " << xdim << "." << endl;
+        return 0;
+    }
+
+    const int max_iterations = bp.args.sizes[2];
+
+    double epsilon  = 0.005;
+
+    auto grid = new double[ydim][xdim];
+    #pragma omp parallel for collapse(2)
+    for(int i=0; i<ydim; i++){      // Initialize the grid
+        for(int j=0;j<xdim;j++){
+            grid[i][j] = 0;
+        }
+    }
+    for(int i=0; i<ydim; i++){      // And borders
+        grid[i][0]      = -273.15;
+        grid[i][xdim-1] = -273.15;
+    }
+    for(int i=0; i<xdim; i++){
+        grid[0][i]      = -273.15;
+        grid[ydim-1][i] = 40.0;
+    }
+
+    bp.timer_start();                                   // Rn
+    solve(grid, epsilon, max_iterations);
     bp.timer_stop();
 
     bp.print("heat_equation(cpp11_omp)");
