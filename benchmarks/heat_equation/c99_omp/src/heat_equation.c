1db8e975b211d3be20928af2a801acd48984f66e
Simon A. F. Lund <safl@safl.dk>
2015-05-20 23:15:02 +0200

    heat_equation-c99_omp: Towards a more comparable/equivalent benchmark.

--- benchmarks/heat_equation/c99_omp/src/heat_equation.c
+++ benchmarks/heat_equation/c99_omp/src/heat_equation.c
@@ -5,6 +5,7 @@
 #include <bp_util.h>
 
 /*
+Without collapsing
 void solve(int height, int width, double *grid, double epsilon, int max_iterations)
 {
     double *T = (double*)malloc(height*width*sizeof(double));
@@ -65,39 +66,33 @@ void solve(int height, int width, double *grid, double epsilon, int max_iteratio
     double delta = epsilon+1.0;
     int iterations = 0;
 
-    while(delta>epsilon) {
+    while (delta>epsilon) {
         ++iterations;
 
-        #pragma omp parallel for shared(grid, T) reduction(+:delta)
-        for(int i=0; i<height-2; ++i)
-        {
-            for(int j=0; j<width-2; ++j)
-            {
+        #pragma omp parallel for reduction(+:delta) collapse(2)
+        for (int i=1; i<height-1; ++i) {
+            for (int j=1; j<width-1; ++j) {
                 const int a = i * width + j;
-                const double *up     = &grid[a+1];
-                const double *left   = &grid[a+width];
-                const double *right  = &grid[a+width+2];
-                const double *down   = &grid[a+1+width*2];
-                const double *center = &grid[a+width+1];
-                double *t_center = &T[a+width+1];
+                const double *up     = &grid[a-width];
+                const double *left   = &grid[a-1];
+                const double *right  = &grid[a+1];
+                const double *down   = &grid[a+width];
+                const double *center = &grid[a];
+                double *t_center = &T[a];
 
-                *t_center = (*center + *up + *left + *right + *down) * 0.2;
+                *t_center = (*up + *down + *center + *left + *right) * 0.2;
                 delta += fabs(*t_center - *center);
             }
         }
 
-        #pragma omp parallel for shared(grid, T)
-        for(int i=0; i<height-2; ++i)
-        {
-            int a = i * width;
-            const double *center = &grid[a+width+1];
-            double *t_center = &T[a+width+1];
+        #pragma omp parallel for collapse(2)
+        for (int i=1; i<height-1; ++i) {
+            for (int j=1; j<width-1; ++j) {
+                const int a = i * width + j;
+                const double *center = &grid[a];
+                double *t_center = &T[a];
 
-            for(int j=0; j<width-2; ++j)
-            {
                 *t_center = *center;
-                ++t_center;
-                ++center;
             }
         }
 
@@ -133,45 +128,27 @@ int main (int argc, char **argv)
     //
     double* grid_i = grid;
     #pragma omp parallel for collapse(2)
-    for(int i=0; i<height; ++i) {
-        for(int j=0; j<width; ++j) {
+    for (int i=0; i<height; ++i) {
+        for (int j=0; j<width; ++j) {
             *grid_i = 0;
             ++grid_i;
         }
     }
     //
     // NumaEffects - end
-    for(int j=0; j<width;j++) {
-        grid[j] = 40.0;
+    for (int j=0; j<height; j++) {
+        grid[j*width]           = -273.15;
+        grid[j*width+width-1]   = -273.15;
+    }
+    for (int j=0; j<width; j++) {
         grid[j+(height-1)*width] = -273.15;
+        grid[j]                  = 40.0;
     }
-    for(int j=1; j<height-1;j++) {
-        grid[j*width] = -273.15;
-        grid[j*width+width-1]= -273.15;
-    }
-#ifdef DEBUG
-    for (int i = 0; i<height; i++)
-    {
-        for(int j=0; j<width;j++)
-        {
-            printf ("%lf ", grid[j+i*width]);
-        }
-        printf ("\n");
-    }       
-#endif
+
     bp.timer_start();
     solve(height, width, grid, epsilon, iter);
     bp.timer_stop();
-#ifdef DEBUG
-    for (int i = 0; i<height; i++)
-    {
-        for(int j=0; j<width;j++)
-        {
-            printf ("%lf ", grid[j+i*width]);
-        }
-        printf ("\n");
-    }       
-#endif
+
     bp.print("heat_equation(c99_omp)");
     free(grid);
     return 0;
