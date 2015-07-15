ce91b53f89a0ebeedb1ef27f88023d1201682548
Simon A. F. Lund <safl@safl.dk>
2015-07-15 13:38:44 +0200

    suites-cpu: Added t8, t24 and t32 suites for cpu.

--- suites/cpu/bp_cpu_shared.py
+++ suites/cpu/bp_cpu_shared.py
@@ -1,6 +1,44 @@
 #
 #   Common / Shared stack configurations for CPU suites.
 #
+bh_stack_cpu_t32_all = [
+    [('default',    'bridge',       None)],
+    [('bccon',      'bccon',        None)],
+    [('bcexp',      'bcexp',        None)],
+    [('topo',       'topological',  None)],
+    [('node',       'node',         None)],
+    [
+        ('cpu_vc_fs_ct_t32',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t16',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "16", "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t08',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t04',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t02',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t01',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0"}),
+
+        ('cpu_vc_fs_t32',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_t16',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "16", "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_t08',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_t04',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_t02',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_t01',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0"}),
+    
+        ('cpu_vc_t32',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t16',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "16", "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t08',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t04',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t02',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t01',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0"}),
+
+        ('cpu_t32',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
+        ('cpu_t16',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "16", "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
+        ('cpu_t08',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
+        ('cpu_t04',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
+        ('cpu_t02',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
+        ('cpu_t01',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
+
+    ]
+]
+
 bh_stack_cpu_t32_best = [
     [('default',    'bridge',       None)],
     [('bccon',      'bccon',        None)],
@@ -40,41 +78,47 @@ bh_stack_cpu_t32 = [
     ]
 ]
 
-bh_stack_cpu_t32_all = [
+bh_stack_cpu_t24 = [
     [('default',    'bridge',       None)],
     [('bccon',      'bccon',        None)],
     [('bcexp',      'bcexp',        None)],
     [('topo',       'topological',  None)],
     [('node',       'node',         None)],
     [
-        ('cpu_vc_fs_ct_t32',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t24',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "24", "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_fs_ct_t16',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "16", "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_fs_ct_t08',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_fs_ct_t04',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_fs_ct_t02',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_fs_ct_t01',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0"}),
-
-        ('cpu_vc_fs_t32',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0"}),
-        ('cpu_vc_fs_t16',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "16", "BH_BCEXP_SIGN": "0"}),
-        ('cpu_vc_fs_t08',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0"}),
-        ('cpu_vc_fs_t04',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0"}),
-        ('cpu_vc_fs_t02',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0"}),
-        ('cpu_vc_fs_t01',       'cpu',  {"BH_CPU_JIT_LEVEL": "2", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0"}),
     
-        ('cpu_vc_t32',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t24',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "24", "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_t16',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "16", "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_t08',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_t04',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_t02',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0"}),
         ('cpu_vc_t01',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0"}),
+    ]
+]
 
-        ('cpu_t32',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
-        ('cpu_t16',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "16", "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
-        ('cpu_t08',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
-        ('cpu_t04',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
-        ('cpu_t02',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
-        ('cpu_t01',             'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0", "BH_CPU_VCACHE_SIZE": "0"}),
-
+bh_stack_cpu_t8 = [
+    [('default',    'bridge',       None)],
+    [('bccon',      'bccon',        None)],
+    [('bcexp',      'bcexp',        None)],
+    [('topo',       'topological',  None)],
+    [('node',       'node',         None)],
+    [
+        ('cpu_vc_fs_ct_t08',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t06',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "6" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t04',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t02',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_fs_ct_t01',    'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0"}),
+    
+        ('cpu_vc_t08',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "8" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t06',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "6" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t04',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "4" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t02',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "2" , "BH_BCEXP_SIGN": "0"}),
+        ('cpu_vc_t01',          'cpu',  {"BH_CPU_JIT_LEVEL": "1", "OMP_NUM_THREADS": "1" , "BH_BCEXP_SIGN": "0"}),
     ]
 ]
 
