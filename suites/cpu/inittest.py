3d7c0d4c7feffd0c2db413f6735417ac09175525
Simon A. F. Lund <safl@safl.dk>
2015-08-31 21:08:14 +0200

    ...

--- suites/cpu/inittest.py
+++ suites/cpu/inittest.py
@@ -4,7 +4,7 @@ scripts = [
     ('Synth Init',   'synth_init',    '--size=100000000*10'),
 ]
 
-stack_omp_t32 = [
+stack_parallel = [
     [('default',    'bridge',       None)],
     [('bccon',      'bccon',        None)],
     [('bcexp',      'bcexp',        None)],
@@ -38,15 +38,37 @@ stack_omp_t32 = [
         ('omp_si_pe_t04',    'cpu',  {"OMP_NUM_THREADS": "4",  "SYNTH_INIT_MODE": 2}),
         ('omp_si_pe_t02',    'cpu',  {"OMP_NUM_THREADS": "2",  "SYNTH_INIT_MODE": 2}),
         ('omp_si_pe_t01',    'cpu',  {"OMP_NUM_THREADS": "1",  "SYNTH_INIT_MODE": 2}),
-        
-        ('omp_si_se',    'cpu',  {"OMP_NUM_THREADS": "1",  "SYNTH_INIT_MODE": 0}),
     ]
 ]
 
-c99 = {
+stack_serial = [
+    [('default',    'bridge',       None)],
+    [('bccon',      'bccon',        None)],
+    [('bcexp',      'bcexp',        None)],
+    [('topo',       'topological',  None)],
+    [('node',       'node',         None)],
+    [
+        ('omp_si_se', 'cpu',  {"SYNTH_INIT_MODE": 0}),
+    ]
+]
+
+parallel    = ('C/OMP',     '`bp-info --benchmarks`/{script}/c99_omp/bin/{script} {args}', None)
+serial      = ('C/SEQ',     '`bp-info --benchmarks`/{script}/c99_seq/bin/{script} {args}', None)
+parallel_ts = ('C/OMP/TS',  'taskset -c 0 `bp-info --benchmarks`/{script}/c99_omp/bin/{script} {args}', None)
+serial_ts   = ('C/SEQ/TS',  'taskset -c 0 `bp-info --benchmarks`/{script}/c99_seq/bin/{script} {args}', None)
+
+serial = {
     'scripts': scripts,
-    'launchers': [c99_omp],
-    'bohrium': stack_omp_t32,
+    'launchers': [serial, serial_ts],
+    'bohrium': stack_serial,
+    "use_slurm_default": True,
+    "use_grapher": "cpu"
+}
+
+parallel = {
+    'scripts': scripts,
+    'launchers': [parallel, parallel_ts],
+    'bohrium': stack_parallel,
     "use_slurm_default": True,
     "use_grapher": "cpu"
 }
@@ -56,6 +78,7 @@ c99 = {
 #   As usual, put them into the list of suites to run.
 #
 suites = [
-    c99
+    serial,
+    parallel
 ]
 
