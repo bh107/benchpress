347b690782c544fdaa71c6605667d4bafb50aec5
Mads R. B. Kristensen <madsbk@gmail.com>
2016-02-09 12:05:55 +0100

    bp-grapher: added --no-legend and fixed the speedup

--- module/benchpress/grapher/bar_per_script.py
+++ module/benchpress/grapher/bar_per_script.py
@@ -30,7 +30,7 @@ def plot(cmds, res, baseline, args):
         err = [v[1] for v in value]
         if len([v for v in avg if v > 0]) == 0:
             raise ValueError("All values to display are zero in '%s'"%comp)
-        c = plt.cm.jet(1. * i / (len(res) - 1))
+        c = plt.cm.jet(1. * i / max(len(res)-1,1))
         b = ax.bar(ind+i*width, avg, width, color=c, log=args.ylog, yerr=err)
         bars.append(b)
         comps.append(texsafe(comp))
@@ -53,14 +53,15 @@ def plot(cmds, res, baseline, args):
     else:
         ax.set_ylabel('%s compared to %s'%(args.data_to_display, baseline))
 
-    ax.legend(bars,
-              comps,
-              ncol=len(comps),
-              loc='upper center',
-              bbox_to_anchor=(0.5, 1.06),
-              fancybox=True,
-              shadow=True,
-              columnspacing=1)
+    if not args.no_legend:
+        ax.legend(bars,
+                  comps,
+                  ncol=len(comps),
+                  loc='upper center',
+                  bbox_to_anchor=(0.5, 1.06),
+                  fancybox=True,
+                  shadow=True,
+                  columnspacing=1)
 
 def get_stack_name(stack):
     names = [comp[0] for comp in stack][1:]
@@ -178,7 +179,8 @@ class Bar_per_script(Graph):
                     values.append(res[script_old][comp_old])
                 except KeyError:
                     values.append((0,0))
-            data.append((comp_new,values))
+            if comp_old != comp_baseline:
+                data.append((comp_new,values))
         plot([s[1] for s in scripts], data, self.args.baseline, self.args)
         self.tofile({"title": self.args.title})
 
