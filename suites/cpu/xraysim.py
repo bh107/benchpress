a5ffaec1e36fd4e510463c7a69d721534c91354c
Simon A. F. Lund <safl@safl.dk>
2016-03-07 06:57:42 +0100

    ...

--- suites/cpu/xraysim.py
+++ suites/cpu/xraysim.py
@@ -6,14 +6,14 @@ scripts = [
     ('X-Ray Simulation (50*20*5)', 'xraysim', '--size=50*20*5'),
     ('X-Ray Simulation (50*63*5)', 'xraysim', '--size=50*63*5'),
     ('X-Ray Simulation (50*200*5)', 'xraysim', '--size=50*200*5'),
-#    ('X-Ray Simulation (50*633*5)', 'xraysim', '--size=50*633*5'),
+    ('X-Ray Simulation (50*633*5)', 'xraysim', '--size=50*633*5'),
 ]
 
 bh_stack_cpu_t32 = [
     [('default',    'bridge',       None)],
     [('bccon',      'bccon',        None)],
     [('bcexp',      'bcexp',        None)],
-    [('topo',       'topological',  None), ('greedy', 'greedy', None)],
+    [('greedy', 'greedy', None)],
     [('node',       'node',         None)],
     [
         ('cpu_vc_fs_ct_t32', 'cpu',  {"BH_CPU_JIT_LEVEL": "3", "OMP_NUM_THREADS": "32", "BH_BCEXP_SIGN": "0"}),
