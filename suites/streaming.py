c7afaee18a1450c0e3bcc9cbc3d20b7886460793
Mads R. B. Kristensen <madsbk@gmail.com>
2016-12-02 15:19:07 +0100

    updated the streaming suite

--- suites/streaming.py
+++ suites/streaming.py
@@ -1,13 +1,13 @@
 from benchpress.default import *
 
 scripts = [
-    ('X-ray 50^3 020^2',  'xraysim',    '--size=50*20*5'),
+    ('X-ray 020^3 40^2',  'xraysim',    '--size=20*40*5'),
     ('MC Pi 0.1G',   'montecarlo_pi',    '--size=100000000*5'),
     ('MFE 040^2',    'idl_init_explode', '--size=40*40*5'),
 
-    ('X-ray 50^3 063^2',  'xraysim',    '--size=50*63*5'),
-    ('X-ray 50^3 200^2',  'xraysim',    '--size=50*200*5'),
-    ('X-ray 50^3 633^2',  'xraysim',    '--size=50*633*5'),
+    ('X-ray 043^3 040^2',  'xraysim',    '--size=43*40*5'),
+    ('X-ray 093^3 040^2',  'xraysim',    '--size=93*40*5'),
+    ('X-ray 200^3 040^2',  'xraysim',    '--size=200*40*5'),
 
     ('MC Pi 1G',    'montecarlo_pi',    '--size=1000000000*5'),
     ('MC Pi 10G',    'montecarlo_pi',    '--size=10000000000*5'),
