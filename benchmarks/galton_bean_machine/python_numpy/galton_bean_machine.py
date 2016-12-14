b3161879bcd792c6270069f03760d728fa66727f
Mads R. B. Kristensen <madsbk@gmail.com>
2016-12-14 10:00:20 +0100

    New benchmark: Galton Bean Machine

---
+++ benchmarks/galton_bean_machine/python_numpy/galton_bean_machine.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from benchpress import util
+import numpy as np
+
+def bean(num_beans, height):
+    return np.sum(np.sign(np.random.random((num_beans,height))-0.5), axis=1)
+
+def main():
+    B = util.Benchmark()
+    num_beans, height = B.size
+
+    B.start()
+    R = bean(num_beans, height)
+    B.stop()
+
+    B.pprint()
+    if B.verbose:
+        print(R)
+    if B.visualize:
+        from matplotlib import pyplot
+        bins   = 100
+        pyplot.hist(R, bins)
+        pyplot.title("Galton Normal distribution")
+        pyplot.xlabel("Value")
+        pyplot.ylabel("Frequency")
+        pyplot.show()
+    if B.outputfn:
+        B.tofile(B.outputfn, {'res': R})
+
+if __name__ == "__main__":
+    main()
---
+++ benchmarks/galton_bean_machine/readme.rst
@@ -0,0 +1,6 @@
+Simulation of the Galton Bean Machine, which was an old physical machine that demonstrated the normal distribution. https://en.wikipedia.org/wiki/Bean_machine
+
+It takes two arguments, the number of beans and the height of the machine::
+
+  --size=10000*100
+
