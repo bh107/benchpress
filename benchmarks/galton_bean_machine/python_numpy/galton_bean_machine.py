ca3ead317cb2d0dc39e432b3ea2d83b9f821ee94
Mads R. B. Kristensen <madsbk@gmail.com>
2017-04-05 11:34:50 +0200

    bean: now using the same random for both NumPy and Bohrium

--- benchmarks/galton_bean_machine/python_numpy/galton_bean_machine.py
+++ benchmarks/galton_bean_machine/python_numpy/galton_bean_machine.py
@@ -2,15 +2,17 @@ from __future__ import print_function
 from benchpress import util
 import numpy as np
 
-def bean(num_beans, height):
-    return np.sum(np.sign(np.random.random((num_beans,height))-0.5), axis=1)
+
+def bean(B, num_beans, height):
+    return np.sum(np.sign(B.random_array((num_beans, height))-0.5), axis=1)
+
 
 def main():
     B = util.Benchmark()
     num_beans, height = B.size
 
     B.start()
-    R = bean(num_beans, height)
+    R = bean(B, num_beans, height)
     B.stop()
 
     B.pprint()
