70090555d18259573230e29d386fba0bc97bf1c8
Mads R. B. Kristensen <madsbk@gmail.com>
2017-04-28 16:45:27 +0200

    NAS-benchmark now uses  ``if __name__ == "__main__"``

--- benchpress/suites/nas_parallel_benchmark.py
+++ benchpress/suites/nas_parallel_benchmark.py
@@ -36,37 +36,39 @@ from os.path import join
 import benchpress as bp
 from benchpress.argument_handling import args, add_argument
 
-# Adding arguments that are specific to this Benchpress suite
-add_argument(
-    '--nas-config',
-    default="make.def.gcc_x86",
-    help="The name of the NAS config to use."
-)
+if __name__ == "__main__":
 
-# Let's download and unpack the NAS benchmark
-tdir = tempfile.mkdtemp()
-nas_filename = join(tdir, "NAS.tgz")
-print ("Download NAS benchmark to '%s'" % nas_filename)
-urllib.urlretrieve("https://www.nas.nasa.gov/assets/npb/NPB3.3.1.tar.gz", nas_filename)
-tar = tarfile.open(nas_filename)
-tar.extractall(path=tdir)
-tar.close()
+    # Adding arguments that are specific to this Benchpress suite
+    add_argument(
+        '--nas-config',
+        default="make.def.gcc_x86",
+        help="The name of the NAS config to use."
+    )
 
-# Choose NAS config
-print ("Using NAS config '%s'" % args().nas_config)
-wdir = join(tdir, "NPB3.3.1/NPB3.3-OMP")
-os.rename("%s/config/NAS.samples/%s" % (wdir, args().nas_config), "%s/config/make.def" % wdir)
+    # Let's download and unpack the NAS benchmark
+    tdir = tempfile.mkdtemp()
+    nas_filename = join(tdir, "NAS.tgz")
+    print ("Download NAS benchmark to '%s'" % nas_filename)
+    urllib.urlretrieve("https://www.nas.nasa.gov/assets/npb/NPB3.3.1.tar.gz", nas_filename)
+    tar = tarfile.open(nas_filename)
+    tar.extractall(path=tdir)
+    tar.close()
 
-benchmarks = ['bt', 'cg', 'dc', 'ft', 'is', 'lu', 'mg', 'sp', 'ua']
-size = "W"
+    # Choose NAS config
+    print ("Using NAS config '%s'" % args().nas_config)
+    wdir = join(tdir, "NPB3.3.1/NPB3.3-OMP")
+    os.rename("%s/config/NAS.samples/%s" % (wdir, args().nas_config), "%s/config/make.def" % wdir)
 
-for benchmark in benchmarks:
-    subprocess.check_call(["make %s CLASS=%s" % (benchmark, size)], shell=True, cwd=wdir)
-print ("BUILD FINISHED\n\n")
+    benchmarks = ['bt', 'cg', 'dc', 'ft', 'is', 'lu', 'mg', 'sp', 'ua']
+    size = "W"
 
-cmd_list = []
-for label in benchmarks:
-    bash_cmd = "{root}/bin/{label}.{size}.x".format(root=wdir, label=label, size=size)
-    cmd_list.append(bp.command(bash_cmd, "%s/%s/OMP1" % (label, size), env={'OMP_NUM_THREADS': 1}))
+    for benchmark in benchmarks:
+        subprocess.check_call(["make %s CLASS=%s" % (benchmark, size)], shell=True, cwd=wdir)
+    print ("BUILD FINISHED\n\n")
 
-bp.create_suite(cmd_list)
+    cmd_list = []
+    for label in benchmarks:
+        bash_cmd = "{root}/bin/{label}.{size}.x".format(root=wdir, label=label, size=size)
+        cmd_list.append(bp.command(bash_cmd, "%s/%s/OMP1" % (label, size), env={'OMP_NUM_THREADS': 1}))
+
+    bp.create_suite(cmd_list)
